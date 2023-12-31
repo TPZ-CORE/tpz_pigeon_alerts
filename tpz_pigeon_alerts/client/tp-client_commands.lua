85d5ab3219ee5ee18a1d7d6a3b9b065da956f654
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2023-12-31 15:43:18 +0200

    Update tp-client_commands.lua
    Added a statement which checks if command is enabled or disabled.

--- tpz_pigeon_alerts/client/tp-client_commands.lua
+++ tpz_pigeon_alerts/client/tp-client_commands.lua
@@ -1,17 +1,21 @@
-RegisterCommand(Config.CallPigeonCommand, function(source, args, rawCommand)
 
-	local player = PlayerPedId()
-	local isDead = IsEntityDead(player)
 
-	if not isDead then
-
-		if ClientData.hasBoughtPigeon then
-			WhistleTrainedPigeon("default")
-		else
-			TriggerEvent('tpz_core:sendRightTipNotification', Locales['NO_TRAINED_PIGEON'], 3000)
+if Config.CallPigeonCommand then
+	RegisterCommand(Config.CallPigeonCommand, function(source, args, rawCommand)
+	
+		local player = PlayerPedId()
+		local isDead = IsEntityDead(player)
+	
+		if not isDead then
+	
+			if ClientData.hasBoughtPigeon then
+				WhistleTrainedPigeon("default")
+			else
+				TriggerEvent('tpz_core:sendRightTipNotification', Locales['NO_TRAINED_PIGEON'], 3000)
+			end
 		end
-	end
-end)
+	end)
+end
 
 RegisterCommand(Config.CancelRouteCommand, function(source, args, rawCommand)
     ClearAlertGpsMultiRoute()
